5c50bf8f57b42aa2703533f57001fd7b2d2a37e3
liugang <lgcf1314@gmail.com>
2022-02-14 09:34:33 +0800

    FIX:220214第一次提交v1.0

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-12-31 23:32:17
- * @LastEditTime: 2022-02-11 09:11:48
+ * @LastEditTime: 2022-02-13 23:17:13
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /d3-tree-example/src/router/index.ts
@@ -80,6 +80,14 @@ const router = new VueRouter({
       // this generates a separate chunk (example.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/ForceExample/force-three.vue'),
+    },
+    {
+      path: '/force-four',
+      name: 'force-four',
+      // route level code-splitting
+      // this generates a separate chunk (example.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/ForceExample/force-four.vue'),
     },
   ],
 })
