9f13dd83c615c8901c12c1b65975476701e7b2c6
liugang <lgcf1314@gmail.com>
2022-04-17 22:37:22 +0800

    fixed

--- src/globals.d.ts
+++
@@ -1,7 +0,0 @@
-declare var $: any
-// eslint-disable-next-line prettier/prettier
-// declare const BMap: any;
-// eslint-disable-next-line prettier/prettier
-declare module '@ssthouse/vue-tree-chart'
-
-interface Window {}
